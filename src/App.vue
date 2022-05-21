dcde8383960a401365d5cce786b26fc4c11e9bbc
luoting <2441369138@qq.com>
2022-05-21 20:23:40 +0800

    feat: 添加代码提交验证

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <HelloWorld />
+    <page1 msg="111"></page1>
+  </div>
+</template>
+
+<script  lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import page1 from './components/page1'
+export default defineComponent({
+  components: {
+    HelloWorld
+  },
+  setup (props) {
+    // 获取组件的实例类型
+    const helloWorld = ref < InstanceType < typeof HelloWorld > | null>(null)
+    onMounted(() => {
+      console.log('hello', helloWorld)
+    })
+    return {
+      HelloWorld
+    }
+  }
+
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    123
+  </div>
+</template>
+
+<script  lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
+interface user {
+  name: String
+  aee: number
+}
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    personInfo: {
+      type: Object,
+      default: () => {}
+    },
+    newuser: {
+      type: Object as user,
+      default: () => {}
+    }
+  },
+  setup (props) {
+    const fool = ref<{
+      a: number
+      b: string
+    } | null>(null)
+    fool.value = {
+      a: 1
+    }
+
+    onMounted(() => {
+      console.log(';;;', fool.value)
+    })
+    return {
+      fool
+    }
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
