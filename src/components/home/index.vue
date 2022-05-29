6d5c40e786d03c188e8a152e688ab39ff1566c5f
luoting <2441369138@qq.com>
2022-05-29 22:11:16 +0800

    引入vuex 添加store类型

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     首页门户
+    {{ store.state.count }}
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { useStore } from '../../store'
+const store = useStore()
+console.log(store.state.count)
 
 </script>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,17 +3,17 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/login',
     name: 'login',
-    component: () => { import('../components/login/index.vue') }
+    component: () => import('../components/login/index.vue')
   },
   {
     path: '/',
     name: 'home',
-    component: () => { import('../components/home/index.vue') }
+    component: () => import('../components/home/index.vue')
   }
 ]
 const router = createRouter({
   history: createWebHashHistory(), // 路由模式
-  routes // 路由规则
+  routes
 
 })
 export default router
